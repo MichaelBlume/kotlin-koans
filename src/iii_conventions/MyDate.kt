046b8ed33b5312b543225ec499f02b1b46d0e774
Michael Blume <michael.blume@climate.com>
2016-01-03 12:03:36 -0800

    make MyDate comparable with MyDate

--- src/iii_conventions/MyDate.kt
+++ src/iii_conventions/MyDate.kt
@@ -1,6 +1,14 @@
 package iii_conventions
 
-data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int)
+data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int) : Comparable<MyDate> {
+    override fun compareTo(other: MyDate): Int {
+        val ydiff = year - other.year
+        if (ydiff != 0) return ydiff
+        val mdiff = month - other.month
+        if (mdiff != 0) return mdiff
+        return dayOfMonth - other.dayOfMonth
+    }
+}
 
 operator fun MyDate.rangeTo(other: MyDate): DateRange = todoTask27()
 
